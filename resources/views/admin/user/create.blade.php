c4d58ef59538a8b9058e21d028a088800c35e7a1
Lev <otselsiy@mail.ru>
2023-06-22 09:13:10 +0300

    add new button for crud
    Signed-off-by: Lev <otselsiy@mail.ru>

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -7,28 +7,28 @@
         @csrf
         <div class="card-body w-25">
             <div class="form-group">
-                <label for="exampleInputEmail1">Фамилия</label>
-                <input type="text" name="surname" class="form-control" id="exampleInputEmail1">
+                <label for="">Фамилия</label>
+                <input type="text" name="surname" class="form-control" id="">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Имя</label>
-                <input type="text" name="name" class="form-control" id="exampleInputEmail1">
+                <label for="">Имя</label>
+                <input type="text" name="name" class="form-control" id="">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Отчество</label>
-                <input type="text" name="middleName" class="form-control" id="exampleInputEmail1">
+                <label for="">Отчество</label>
+                <input type="text" name="middleName" class="form-control" id="">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Email</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1">
+                <label for="">Email</label>
+                <input type="email" name="email" class="form-control" id="">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Логин</label>
-                <input type="text" name="login" class="form-control" id="exampleInputEmail1">
+                <label for="">Логин</label>
+                <input type="text" name="login" class="form-control" id="">
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Пароль</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                <label for="">Пароль</label>
+                <input type="password" name="password" class="form-control" id="">
             </div>
         </div>
 
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -2,26 +2,43 @@
 @section('title', 'Пользователи')
 
 @section('content')
-    <div class="d-flex align-items-center justify-content-between pr-2 h-50 mb-3">
+    <div class="d-flex align-items-center justify-content-between pr-2 mb-3">
         <span class="fw-bolder">Управление пользователями</span>
         <div><a href="{{ route('admin.user.create') }}" class="btn btn-block btn-outline-primary btn-sm">Создать</a>
         </div>
     </div>
     @foreach($users as $user)
-        <div class="d-flex justify-content-between align-items-center mb-2">
-            <div>{{ $user->name }}</div>
-            <div class="d-flex justify-content-end">
-                <a href=""
-                   class="btn btn-primary me-3">Просмотреть</a>
-                <a href=""
-                   class="btn btn-primary me-3">Редактировать</a>
-                <a href=""
-                   class="btn btn-primary me-3">Роль</a>
-                <form action="" method="POST">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" class="btn btn-danger" value="Удалить">
-                </form>
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="user-panel d-flex align-items-center">
+                <div class="image">
+                    <img src="{{ asset('image102-10.jpg') }}" class="img-circle object-fit-contain" alt="User Image">
+                </div>
+                <div class="info">
+                    <div class="col-1">
+                        <div class="row-1 text-dark">{{ $user->surname }} {{ $user->name }}</div>
+                        <div class="row-1"><span class="badge bg-danger">Администратор</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="info p-2 d-flex align-items-end">
+                <div class="col">
+                    <a href="{{ route('main.index') }}" class="btn btn-light" data-bs-toggle="tooltip"
+                       data-bs-html="true" data-bs-placement="top" title="Просмотреть информацию о пользователе">
+                        <i class="fas fa-eye"></i>
+                    </a>
+                    <a href="{{ route('main.index') }}" class="btn btn-light" data-bs-toggle="tooltip"
+                       data-bs-html="true" data-bs-placement="top" title="Редактировать">
+                        <i class="fas fa-edit"></i>
+                    </a>
+                    <a href="{{ route('main.index') }}" class="btn btn-light" data-bs-toggle="tooltip"
+                       data-bs-html="true" data-bs-placement="top" title="Изменить роль">
+                        <i class="fas fa-user-tag"></i>
+                    </a>
+                    <a href="{{ route('main.index') }}" class="btn btn-light" data-bs-toggle="tooltip"
+                       data-bs-html="true" data-bs-placement="top" title="Удалить">
+                        <i class="fas fa-trash-alt text-danger"></i>
+                    </a>
+                </div>
             </div>
         </div>
     @endforeach
