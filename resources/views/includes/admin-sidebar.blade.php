6549dc62e1ee027d37c2794b06f22b00a034b3c9
Lev <otselsiy@mail.ru>
2023-06-24 13:20:59 +0300

    Update design
    Signed-off-by: Lev <otselsiy@mail.ru>

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -23,6 +23,12 @@
                     </a>
                 </li>
                 <li class="nav-item">
+                    <a href="{{ route('admin.index') }}" class="nav-link">
+                        <i class="fas fa-user-cog mr-2"></i>
+                        <p>Администрирование <i class="fas fa-angle-right right"></i></p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="{{ route('admin.user.index') }}" class="nav-link">
                         <i class="fas fa-users-cog mr-2"></i>
                         <p>Пользователи <i class="fas fa-angle-right right"></i></p>
