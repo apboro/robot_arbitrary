9a72fcb4d0127026030df81f3f28721bcfb729e9
Lev <otselsiy@mail.ru>
2023-06-27 17:33:57 +0300

    update design
    Signed-off-by: Lev <otselsiy@mail.ru>

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>ФКПОУ "КТИ" Минтруда России &copy; 2023.</strong>
     Все права защищены.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Версия</b> 0.4.2
+        <b>Версия</b> 0.4.4
     </div>
 </footer>
--- resources/views/includes/no-data.blade.php
+++ resources/views/includes/no-data.blade.php
@@ -5,5 +5,5 @@
 {{--    </blockquote>--}}
 {{--</figure>--}}
 <div class="d-flex p-3 border border-1 rounded-3 bg-light">
-    <span style="font-size: 16px;"><i class="fas fa-ban"></i> Записи не найдены</span>
+    <span style="font-size: 16px;"><i class="fas fa-ban mr-2"></i> Записи не найдены</span>
 </div>
