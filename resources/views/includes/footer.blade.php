6d1bf60fadfdb151d1dbc7b7f29ec8bc5df56824
Lev <otselsiy@mail.ru>
2023-06-30 22:57:49 +0300

    Create environment in `.env` `APP_VERSION` and create environment in `config/app` => 'version'
    Signed-off-by: Lev <otselsiy@mail.ru>

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>ФКПОУ "КТИ" Минтруда России &copy; 2023.</strong>
     Все права защищены.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Версия</b> 0.5.5
+        <b>Версия</b> {{ config('app.version', 'The application version is not defined') }}
     </div>
 </footer>
